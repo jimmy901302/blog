f1f44f4472330055b805bab496fa0d9fde9a5e19
jimmy901302 <bohnjhi@gmail.com>
2026-02-01 01:52:09 +0800

    111

---
+++ src/data/links.ts
@@ -0,0 +1,17 @@
+import TwitterIcon from '@/components/icons/TwitterIcon'
+import GithubIcon from '@/components/icons/GithubIcon'
+
+// ADD YOUR SOCIAL NETWORKS HERE
+export const SOCIALNETWORKS = [
+	{
+		name: 'Github',
+		url: '#',
+		icon: GithubIcon
+	},
+
+	{
+		name: 'Twitter',
+		url: '#',
+		icon: TwitterIcon
+	}
+] as const
